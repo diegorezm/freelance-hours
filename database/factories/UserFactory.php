e30fc03cb7b7b99469060bd32670b78aaa390e39
diegorezm <dmourarezende@gmail.com>
2024-10-08 10:02:45 -0300

    users + projects

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'avatar' => 'https://avatar.iran.liara.run/public',
+            'rating' => fake()->numberBetween(0, 5),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
